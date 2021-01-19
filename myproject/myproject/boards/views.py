d6711e140d25d553984f07c2f7c75d66020ed936
CeciliaRios <38302840+CMRR1@users.noreply.github.com>
2021-01-18 21:17:25 -0700

    Board Project(2/6)
    Use to models. migrations and templetes.
    Install boostrap and use the admin mode.

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Board
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, World!')
+  
+  
+  #  boards = Board.objects.all()
+  #  boards_names=list()
+
+    # for board in boards:
+    # boards_names.append(board.name)
+
+    #response_html='<br>'.join(boards_names)
+
+    #return HttpResponse(response_html)
+
+    #UPDATE
+    boards=Board.objects.all()
+    return render(request, 'home.html', {'boards':boards})
+
